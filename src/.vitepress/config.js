d7810ec5ad3b524485f0b4c876540637126cb55f
Ghostasoul <yushuo6174@126.com>
2024-03-21 21:37:59 +0800

    feat(全部): 修复了项目放到GitHub Pages上样式不生效的问题
    需要在config.js中设置base的值为项目值

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -5,12 +5,14 @@ export default {
   titleTemplate: ':title - Custom Suffix',
   description: 'Just playing around.',
   //设置防止md文件的根目录
-  //srcDir: './src',
+  //srcDir: './myvitpress',
   //是否清除后缀的html
-  cleanUrls: true,
+  cleanUrls: false,
   //白色和暗黑模式，默认为 true，可以设置为 false
   appearance: true,
   lastUpdated: true,
+  //设置这个值是为了让项目放到GitHub Pages上也有样式，这个值是GitHub项目的名称
+  base: "/myvitpress/",
   themeConfig: {
     // 主题级选项
     // https://vitepress.dev/reference/default-theme-config
--- src/readme.md
+++ src/readme.md
@@ -11,6 +11,8 @@ npm run dev
 ```
 
 ### 部署到GitHub：
+`部署到的是myvitpress项目中的blog分支`
 ```shell
 npm run deploy
 ```
+> 相关部署说明：https://blog.csdn.net/qq_41295954/article/details/129287652
